6e02817f61ec3b14448ca37126f8becd002a81ee
birobalint95 <87605943+birobalint95@users.noreply.github.com>
2021-08-23 17:16:16 +0200

    Create dark_mode.js

---
+++ dark_mode.js
@@ -0,0 +1,7 @@
+// On toggler clicked
+
+var container = document.getElementsByClassName('container')[0];
+document.getElementById('toggler').addEventListener('change', (event) => {
+  console.log(toggler)
+  event.target.checked ? container.removeAttribute('data-theme') : container.setAttribute('data-theme', 'dark');
+});
