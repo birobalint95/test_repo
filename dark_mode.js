4f3fde400fbe5cf0ad9fe9b8ee87b363fb03404f
birobalint95 <87605943+birobalint95@users.noreply.github.com>
2021-08-24 09:24:58 +0200

    Update dark_mode.js

--- dark_mode.js
+++ dark_mode.js
@@ -5,3 +5,8 @@ document.getElementById('toggler').addEventListener('change', (event) => {
   console.log(toggler)
   event.target.checked ? container.removeAttribute('data-theme') : container.setAttribute('data-theme', 'dark');
 });
+
+document.getElementById('toggler2').addEventListener('change', (event) => {
+  console.log(toggler2)
+  event.target.checked ? container.removeAttribute('data-theme') : container.setAttribute('data-theme', 'dark');
+});
