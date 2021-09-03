21256f1d3acb58beb89fab6dac802a9526667661
birobalint95 <87605943+birobalint95@users.noreply.github.com>
2021-09-03 14:37:43 +0200

    Update dark_mode.js

--- dark_mode.js
+++ dark_mode.js
@@ -31,23 +31,58 @@ function selectNavElement(clicked_id) {
 }
 
 
+// get label selector checkbox elements
 var labelSelectorCheckboxes = document.getElementsByClassName("labelSelectorCheckbox");
-var thoughtBubbles = document.getElementsByClassName("thought-bubble");
 
-var myFunction = function(event) {
-	var checkboxText = event.target.getAttribute("value");
+// create label selector function that gets triggered by an event
+var labelSelectorFunction = function(event) {
+	// get number of checked label selector checkboxes
+	var numberOfCheckedLabelSelectorCheckboxes = document.querySelectorAll('.labelSelectorCheckbox:checked').length;
+	// check if there were no checked label selector checkboxes
+	if (numberOfCheckedLabelSelectorCheckboxes == 0) {
+		// create a span element for the warning message of no selected label
+		var noSelectedLabelWarningMessage = document.createElement('span')
+		// define the id of the span
+		noSelectedLabelWarningMessage.id = "no-selected-label-warning-message";
+		// define the text the warning message displays
+		noSelectedLabelWarningMessage.innerHTML = "No thoughts found. Please check if you have any of the labels selected above.";
+		// get label checkbox container element and append the warning message element to it
+		document.getElementById("label-checkbox-container").appendChild(noSelectedLabelWarningMessage);
+	} else {
+		// create the span element for the warning message of no selected label
+		var noSelectedLabelWarningMessage = document.getElementById("no-selected-label-warning-message");
+		// check if the element exists
+		if (noSelectedLabelWarningMessage) {
+			// if it does, remove it
+			noSelectedLabelWarningMessage.remove();
+		}
+	}
+	// get thought bubble elements
+	var thoughtBubbles = document.getElementsByClassName("thought-bubble");
+	// get value attribute of the checkbox that triggered the function
+	var actualCheckboxValue = event.target.getAttribute("value");
+	// define the variable for indexing thought bubbles
 	var indexOfThoughtBubble;
+	// loop through the thought bubbles
 	for (indexOfThoughtBubble = 0; indexOfThoughtBubble < thoughtBubbles.length; indexOfThoughtBubble++) {
-		if (thoughtBubbles[indexOfThoughtBubble].innerText.includes(checkboxText.toUpperCase())) {
+		// check if the inner text of the actual thought bubble includes the checkbox value (in a proper format)
+		if (thoughtBubbles[indexOfThoughtBubble].innerText.includes(actualCheckboxValue.toUpperCase())) {
+			// if it does, check if the checkbox is checked
 			if (event.target.checked) {
+				// if it is, then display the actual thought bubble as a block
 				thoughtBubbles[indexOfThoughtBubble].style.display = "block";
+			// else, if the checkbox is unchecked
 			} else {
+				// do not display the actual thought bubble
 				thoughtBubbles[indexOfThoughtBubble].style.display = "none";
 			}
 		}
 	}
 };
 
-for (var i = 0; i < labelSelectorCheckboxes.length; i++) { 
-	labelSelectorCheckboxes[i].addEventListener('click', myFunction, false);
+// loop through the label selector checkbox elements
+for (var i = 0; i < labelSelectorCheckboxes.length; i++) {
+	// add event listeners to clicking on these checkboxes
+	labelSelectorCheckboxes[i].addEventListener('click', labelSelectorFunction, false);
 }
+
